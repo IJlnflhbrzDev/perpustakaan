fd81b5d5afe84e3b9ccf8c0167cafbd659b511f8
IJlal Naufal Hibrizi <IJlnflhbrzDev@gmail.com>
2023-10-03 18:11:54 +0700

    first commit ea

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.1
+    </div>
+    <strong>Perpustakaan &copy; <?= date('Y'); ?>. Online </strong> SMKN 1 Cikarang Barat. |  <a href='https://sharecodingan.blogspot.com/' title='sharecodingan.blogspot.com' target='_blank'></a>
+    
+</footer>
